9e3b748bad219ed497c9ce8e274d892ceb525a6b
MaximilianoPintos98 <pintosmaxii98@gmail.com>
2022-08-22 23:05:16 -0300

    Mejoras y Optimizacion 22082022

--- src/components/App/Home/Home.js
+++ src/components/App/Home/Home.js
@@ -1,23 +1,10 @@
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 import { MatchCard } from "../MatchCard/MatchCard";
-
-import { config } from "./../../../routers/AxiosConfig";
 
 import "./Home.css";
 
+
 export const Home = () => {
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       `https://v3.football.api-sports.io/fixtures`,
-  //       config
-  //     )
-  //     .then((response) => {
-  //       console.log(response.data.response);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
 
   return (
     <div className="container">
